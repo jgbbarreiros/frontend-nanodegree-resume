82bcfe89b80fd8cde925ec7fd837059e58f326b4
Joao Barreiros <jgbbarreiros@gmail.com>
2015-07-11 14:49:20 +0100

    Finish lesson 1 Javascript objects (bio, education, work and projects) Fixed index

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,91 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "John Doe",
+    "role": "Web Developer",
+    "contact": {
+        "mobile": "650-555-5555",
+        "email": "john@example.com",
+        "github": "johndoe",
+        "twitter": "@johndoe",
+        "location": "San Francisco"
+    },
+    "welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc.",
+    "skills": [
+        "awesomesess", "delivering things", "cryogenic sleep", "saving the universe"
+    ],
+    "bioPic": "images/fry.jpg"
+};
+
+var education =  {
+    "schools": [
+        {
+            "name": "Nova Southeastern University",
+            "location": "Fort Lauderdale, FL",
+            "degree": "Masters",
+            "major": ["CS"],
+            "dates": 2013,
+            "url": "https://example.com"
+        },
+        {
+            "name": "Eckerd College",
+            "location": "Saint Petersburg, FL",
+            "degree": "BA",
+            "major": ["CS", "French"],
+            "dates": 2003,
+            "url": "https://example.com"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "JavaScript Crash Course",
+            "school": "Udacity",
+            "dates": 2014,
+            "url": "https://www.udacity.com/course/ud804"
+        }
+    ]
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Planet Express",
+            "title": "Delivery Boy",
+            "dates": "January 3000 - Future",
+            "description": "Who moved my cheese cheesy feet cauliflower" +
+            " cheese. Queso taleggio when the cheese comes out everybody's" +
+            " happy airedale ricotta cheese and wine paneer camembert de" +
+            " normandie. Swiss mozzarella cheese slices feta fromage frais" +
+            " airedale swiss cheesecake. Hard cheese blue castello halloumi" +
+            " parmesan say cheese stinking bishop jarlsberg."
+        },
+        {
+            "employer": "Panucci's Pizza",
+            "title": "Delivery Boy",
+            "dates": "1998 - December 31, 199",
+            "description": "Who moved my cheese cheesy feet cauliflower" +
+            " cheese. Queso taleggio when the cheese comes out everybody's" +
+            " happy airedale ricotta cheese and wine paneer camembert de" +
+            " normandie. Swiss mozzarella cheese slices feta fromage frais" +
+            " airedale swiss cheesecake. Hard cheese blue castello halloumi" +
+            " parmesan say cheese stinking bishop jarlsberg."
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "Sample Project 1",
+            "dates": 2014,
+            "description": "Who moved my cheese cheesy feet cauliflower" +
+            " cheese. Queso taleggio when the cheese comes out everybody's" +
+            " happy airedale ricotta cheese and wine paneer camembert de" +
+            " normandie. Swiss mozzarella cheese slices feta fromage frais" +
+            " airedale swiss cheesecake. Hard cheese blue castello halloumi" +
+            " parmesan say cheese stinking bishop jarlsberg.",
+            "images": [
+                "https://image.com/imagename.jpg",
+                "https://image.com/imagename2.jpg"
+            ]
+        }
+    ]
+};
